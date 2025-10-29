a8fe014102606d8d9f927024e7ca0b5d897ae614
LAPTOP-SRREC15G\Atiruj <atiruj.mik@mahidol.ac.th>
2025-10-29 14:08:28 +0700

    build before release

--- dist/nodes/PdfToImg/PdfToImg.node.d.ts
+++ dist/nodes/PdfToImg/PdfToImg.node.d.ts
@@ -1,18 +1,11 @@
-declare const Console: any;
-declare var __createBinding: any;
-declare var __setModuleDefault: any;
-declare var __importStar: any;
-declare var __importDefault: any;
-declare const pdfPoppler: any;
-declare const path: any;
-declare const fs: any;
-declare class PdfToImg {
-    constructor();
-    convertPdftoImages(pdfBuffer: any): Promise<{
-        data: any;
-        filename: any;
-        pageNumber: number;
-        totalPages: any;
-    }[]>;
-    execute(): Promise<any>;
+import {
+	INodeType,
+	INodeTypeDescription,
+	IExecuteFunctions,
+	INodeExecutionData,
+} from 'n8n-workflow';
+
+export declare class PdfToImg implements INodeType {
+	description: INodeTypeDescription;
+	execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
 }
